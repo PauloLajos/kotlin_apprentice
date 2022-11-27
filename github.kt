e89be31c74e62c1023de4000e97b39399dfa8014
Lajos Paulo <paulolajos@mail.com>
2022-11-27 06:01:13 +0100

    restart

--- github.kt
+++ github.kt
@@ -1,6 +1,11 @@
 git config --global user.name "Lajos Paulo"
 git config --global user.email "paulolajos@mail.com"
 
+git init
+git remote add origin https://github.com/PauloLajos/kotlin_apprentice.git
+git branch -M main
+git pull --rebase origin main
+
 git add .
 git commit -m "My commit message"
-git push -u origin HEAD
+git push origin main
